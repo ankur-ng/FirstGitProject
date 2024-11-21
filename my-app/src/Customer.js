6b07a42c40e7879a96eab66092b06c13c461f529
Ankur-ng <ankurchoudhary4u@gmail.com>
2024-11-22 00:39:50 +0530

    11-22-2024 work commit

--- my-app/src/Customer.js
+++ my-app/src/Customer.js
@@ -1,60 +1,50 @@
-import React,{Component} from 'react'
+import React, { Component } from 'react'
 import axios from 'axios';
 
 
 export default class Customer extends Component {
-   
-   constructor(props){
-    super(props)
-    debugger
-    this.state = {Studen: []};
- 
 
-    axios.get('assets/Student.json').then(response => {
+   constructor(props) {
+      super(props) // this is super class constructor
       debugger
-      this.setState({ Studen: response })
-      })
-    
+
+
+      this.state = {
+         data1: "Hello ankur",
+         data2: "Hello sagar",
+         data3: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+      }
+      // axios.get('assets/Student.json').then(response => {
+      //    debugger
+
+      // })
    }
 
-   render(){
-        const students = this.state.Studen.map((item, i) => (
+   remover=()=>{
+      debugger
+     // this.state.data3 =[]
+      this.setState({data3 : [], data2:"ooooooooooooooooooooooooooooo"})
+   }
+
+   addLI=()=>{
+    this.setState({data3 :[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]})
+   }
+
+  
+
+   render() {
+      return (
+         <div>
+            <div>{this.state.data1}</div>
+            <div><input type='text' value={this.state.data2}></input></div>
+            <ul>
+               {this.state.data3.map((item, index) => (<li>index is - {index} and item is :{item}</li>))}
+            </ul>
             <div>
-              <h1>{ item.ID }</h1>
-              <span>{ item.Name }, { item.Address }</span>
+               <button onClick={()=>this.remover()}>remove LI</button>
+               <button onClick={()=>this.addLI()}>add LI</button>
             </div>
-          ));
-      
-          return (
-            <div id="layout-content" className="layout-content-wrapper">
-              <div className="panel-list">{ students }</div>
-            </div>
-          );
-
-    
-}
+         </div>
+      );
    }
-
-//    getDerivedStateFromProps(){
-//     debugger
-//    }
-// shouldComponentUpdate(){
-//     debugger
-// }
-
-// getSnapshotBeforeUpdate() {
-//     debugger
-// }
-
-// componentDidUpdate()
-// {
-// debugger
-// }
-
-//}
-
-// class Studen{
-//     myId;
-//     myName;
-//     myAddress;
-// }
+}
---
+++ my-app/src/Study.js
@@ -0,0 +1,19 @@
+var aRR =[];
+var rts = ""// it's string type varibale with name of rts
+var intData = 465
+
+var data =new Object() //{}, data is object type of variable. and object is json(javascript object) type
+data.ID="123";
+data.Name="Sagar";
+data.Address ="Bangalore"
+
+var data = {"ID":"123",
+    "Name":"Sagar",
+    "Address": "Bangalore"}
+
+var obj23 = new Sagar() // class object
+
+var obj1 = new Math()
+obj1.name = "Sagar";
+obj1.ronumber = "132";
+
